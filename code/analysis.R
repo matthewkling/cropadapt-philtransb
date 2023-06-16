a0574f1fbbb90a9e6b661ea28b18050b38bf46b5
Matthew Kling <mattkling@gmail.com>
2023-06-16 14:36:13 -0400

    add stat tests for revision

--- code/analysis.R
+++ code/analysis.R
@@ -109,11 +109,13 @@ county_size <- county %>% group_by(fips) %>% summarize(acres = mean(acres))
 td <- trends %>%
         right_join(county_lm) %>%
         right_join(county_size) %>%
-        filter(var %in% c("cwd", "aet"))
+        filter(var %in% c("cwd", "aet")) %>%
+        mutate(variable = factor(var, levels = c("aet", "cwd"),
+                                 labels = c("actual evapotranspiration", "climatic water deficit")))
 
 # trend summaries 
 tsd <- td %>%
-        group_by(var) %>%
+        group_by(var, variable) %>%
         summarize(clim_delta = weighted.mean(clim_delta, acres, na.rm = T),
                   crop_trend = weighted.mean(crop_trend, acres, na.rm = T))
 
@@ -122,16 +124,16 @@ tsd <- td %>%
 # figures =========================================================================
 
 # scatterplots
-pxx <- td %>%
+scats <- td %>%
         mutate(var = toupper(var)) %>%
         ggplot(aes(clim_delta, crop_trend, weight = acres)) +
-        facet_wrap(~ var, scales = "free", ncol = 1) +
+        facet_wrap(~ variable, scales = "free", ncol = 1) +
         geom_point(aes(color = sign(clim_delta) == sign(crop_trend)), size = .5) +
         geom_hline(yintercept = 0, color = "gray") +
         geom_vline(xintercept = 0, color = "gray") +
-        geom_vline(data = tsd %>% mutate(var = toupper(var)), aes(xintercept = clim_delta), 
+        geom_vline(data = tsd, aes(xintercept = clim_delta), 
                    color = "black") +
-        geom_hline(data = tsd %>% mutate(var = toupper(var)), aes(yintercept = crop_trend), 
+        geom_hline(data = tsd, aes(yintercept = crop_trend), 
                    color = "black") +
         geom_smooth(method = "lm", color = "black", se = F) +
         scale_color_manual(values = c("darkred", "dodgerblue")) +
@@ -140,17 +142,17 @@ pxx <- td %>%
         theme_minimal() +
         theme(legend.position = "none") +
         labs(y = "trend in farm climate index", 
-             x = "trend in climate variable (mm/yr)")
+             x = "trend in climate variable")
 
 # maps
-px <- counties %>%
+maps <- counties %>%
         mutate(fips = paste0(STATEFP, COUNTYFP)) %>%
         left_join(trends) %>%
         left_join(td) %>%
-        filter(!is.na(var)) %>%
+        filter(!is.na(variable)) %>%
         mutate(var = toupper(var)) %>%
         ggplot(aes(fill = sign(clim_delta) == sign(crop_trend))) +
-        facet_wrap(~ var, ncol = 1) +
+        facet_wrap(~ variable, ncol = 1) +
         geom_sf(data = counties, fill = "gray90", color = NA) +
         geom_sf(color = NA) +
         scale_x_continuous(limits = c(-125, -66), expand = c(0, 0)) +
@@ -161,7 +163,7 @@ px <- counties %>%
         labs(fill = "signs of climate and crop trends match")
 
 # combine plots and save to disk
-p <- pxx + px + guide_area() +
+p <- scats + maps + guide_area() +
         plot_layout(design = c("AB
                                CC"), nrow = 2, widths = c(1, 2), heights = c(10, 1),
                     guides = "collect") 
@@ -169,3 +171,51 @@ ggsave("outputs/figure_3.png",
        p, width = 8, height = 6.5, units = "in")
 
 
+
+# hypothesis testing =========================================================================
+
+# data logging function
+path <- "outputs/stats.txt"
+if(file.exists(path)) file.remove(path)
+record <- function(x, preamble = NULL, summarize = F){
+        sink(path, append = T)
+        if(!is.null(preamble)) cat(c("\n", preamble, "\n\n"))
+        print(x)
+        sink()
+}
+
+# construct neighbors list
+nb <- counties %>%
+        filter(fips %in% td$fips) %>%
+        arrange(fips) %>%
+        spdep::poly2nb() %>%
+        spdep::nb2listw(zero.policy = TRUE)
+
+# prep data for regression models
+datasets <- c("aet", "cwd") %>%
+        map(function(x) td %>% 
+                    filter(var == x) %>% 
+                    arrange(fips) %>% 
+                    mutate(clim_delta = clim_delta - mean(clim_delta))) %>% # centering predictor so intercept == mean(y)
+        setNames(c("aet", "cwd"))
+
+# function to check whether residuals of a regresison model are autocorrelated
+resid_autocorr <- function(fit, nbl = nb, alpha = 0.05){
+        fit %>%
+                pluck("residuals") %>%
+                moran.test(nbl, zero.policy = TRUE)
+}
+
+# standard OLS regression models
+fits <- datasets %>%
+        map(function(x) lm(crop_trend ~ clim_delta, data = x, weight = acres))
+fits %>% map(summary) %>% record("STANDARD OLS MODELS, regression summaries:")
+fits %>% map(resid_autocorr) %>% record("STANDARD OLS MODELS, residual autocorrelation tests:")
+
+# fit spatial error regression models (since the prior models do have autocorrelated errors)
+sperr_fits <- datasets %>% 
+        map(function(x) errorsarlm(crop_trend ~ clim_delta,
+                                   data = x, weights = acres, listw = nb,
+                                   zero.policy = TRUE, na.action = na.omit))
+sperr_fits %>% map(summary) %>% record("SPATIAL ERROR MODELS, regression summaries:") 
+sperr_fits %>% map(resid_autocorr) %>% record("SPATIAL ERROR MODELS, residual autocorrelation tests:")
